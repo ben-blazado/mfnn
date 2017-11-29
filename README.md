f193e11f17401455ad1dce939c3f1e19fb3078e4
ben-blazado <rx8dr1vr3@gmail.com>
2017-11-29 12:58:09 -0700

    updated look of graphs

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # mfnn
 
 mfnn is a neural network that trains on bike usage, weather, holiday, and  and estimates future bike usage. The data is from the UCI Machine Learning Respository Bike Sharing Dataset. This is a refactored version the Udacity project "My First Neural Network" of the Machine Learning Foundations Program.
+
+# usage
+
+open mfnn.ipynb in jupyter notebook to see training and prediction accuracy of mfnn.
--- mfnn_plot.py
+++ mfnn_plot.py
@@ -1,7 +1,9 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 def plot_losses (training_losses, validation_losses):
     #--- display the losses on a graph
+    plt.figure(figsize=(10,6))
     plt.plot(training_losses, label='Training loss')
     plt.plot(validation_losses, label='Validation loss')
     plt.legend()
@@ -9,16 +11,33 @@ def plot_losses (training_losses, validation_losses):
     plt.title("Loss over Epoch")
     plt.xlabel("Epoch")
     plt.ylabel("Loss")
-    plt.figure(figsize=(8,4))
     return
 
-def plot_predictions(predicted_values, target_values):
 
+def plot_predictions_bar(predicted_values, target_values):
+
+    x_values = np.arange(len(predicted_values))
+    bar_width = 0.4
+    
+    plt.figure(figsize=(10,6))
+    plt.bar(x_values, predicted_values, width=bar_width, label='Predicted')
+    plt.bar(x_values + bar_width, target_values, width=bar_width, label='Actual')
+    plt.legend()
+    plt.title("Predicted v. Actual Bike Usage")
+    plt.xlabel("Date")
+    plt.ylabel("Bike Usage")
+    plt.xticks(x_values + bar_width / 2, x_values)
+    
+    return
+
+
+def plot_predictions_line(predicted_values, target_values):
+
+    plt.figure(figsize=(10,6))
     plt.plot(target_values, label='Actual')
     plt.plot(predicted_values, label='Predicted')
     plt.legend()
     plt.title("Predicted v. Actual Bike Usage")
     plt.xlabel("Date")
     plt.ylabel("Bike Usage")
-    plt.figure(figsize=(8,4))
     return
