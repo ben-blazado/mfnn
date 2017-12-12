7ca2b5c1fedd97ebf076e4e1d0d34e68fa9d8c51
ben-blazado <rx8dr1vr3@gmail.com>
2017-12-11 22:13:45 -0700

    updated date/hr labels on x-axis of predictions graph

--- mfnn_plot.py
+++ mfnn_plot.py
@@ -1,6 +1,9 @@
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from  matplotlib.dates import date2num
 #from matplotlib.ticker import MultiplerLocator
 from datetime import datetime
+from datetime import timedelta  
 import numpy as np
 
 figsize = (10,6)
@@ -20,7 +23,39 @@ def plot_losses (training_losses, validation_losses):
     
 def plot_predictions_bar(predicted_values, target_values, datetimes):
 
-    #---conver datetimes (['date_str', hr]) into datetime format
+    #---conver datetimest (['date_str', hr]) into datetime format
+    datetimes = [datetime.strptime(date_str + ' ' + str(hr), "%Y-%m-%d %H") for [date_str, hr] in datetimes]
+    
+    fig, ax = plt.subplots(figsize=figsize)
+    
+    ax.grid(True, which='both', axis='y')
+
+    x_values = date2num(datetimes)
+    bar_width = (1/len(predicted_values)) / 2
+    ax.bar(x_values - bar_width /2.5 , predicted_values, width=bar_width/1.25, label='Predicted')
+    ax.bar(x_values + bar_width /2.5, target_values, width=bar_width/1.25, label='Actual')
+    ax.set_xlim(x_values[0] - bar_width /1.25, x_values[-1] + bar_width /1.25)
+
+
+    ax.xaxis_date()
+    ax.xaxis.set_tick_params(which='major', pad=15)    
+    ax.xaxis.set_major_locator(mdates.DayLocator())
+    ax.xaxis.set_major_formatter(mdates.DateFormatter("%b-%d"))
+    ax.xaxis.set_minor_locator(mdates.HourLocator())
+    ax.xaxis.set_minor_formatter(mdates.DateFormatter("%H"))
+    fig.autofmt_xdate(rotation=90, ha='center')    
+    
+    plt.legend()
+    plt.title("Predicted v. Actual Bike Usage")
+    plt.xlabel("Date/Hour")
+    plt.ylabel("Bike Usage")
+    
+    return
+
+    
+def plot_predictions_bar_old(predicted_values, target_values, datetimes):
+
+    #---conver datetimest (['date_str', hr]) into datetime format
     datetimes = [datetime.strptime(date_str + ' ' + str(hr), "%Y-%m-%d %H") for [date_str, hr] in datetimes]
     x_values = np.arange(len(predicted_values))
     bar_width = 0.4
