0866809a0ec4085f4e5267142e6f45128b89d9e2
ben-blazado <rx8dr1vr3@gmail.com>
2017-12-14 14:42:17 -0700

    updated second graph to show dates

--- mfnn_graph.py
+++ mfnn_graph.py
@@ -1,9 +1,7 @@
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 from  matplotlib.dates import date2num
-#from matplotlib.ticker import MultiplerLocator
-from datetime import datetime
-from datetime import timedelta  
+from datetime import datetime, timedelta
 import numpy as np
 
 figsize = (10,6)
@@ -25,17 +23,17 @@ def plot_predictions_24hr (predicted_values, target_values, datetimes, str_selec
     #--- convert str_selected_date to datetime format
     selected_date = datetime.strptime(str_selected_date, "%Y-%m-%d").date()
     
-    #--- create an array of indexes for the selected date
+    #--- create an array of indexes, idx_selected, from datetimes that correspond to selected date
     dates = [d.date() for d in datetimes]
     idx_selected = np.squeeze (np.argwhere(np.array(dates) == selected_date))
     
-    #--- select the values based on idx_selected
+    #--- select the values based on the indexes in idx_selected
     selected_predicted_values = predicted_values[idx_selected]
     selected_target_values    = target_values[idx_selected]
     selected_datetimes        = datetimes[idx_selected]
     
     #--- the values on the x-axis will be the selected date times
-    #--- the date times need to be converted to numbers to calculate position of each bar on x-axis
+    #--- the datetimes need to be converted to numbers to calculate position of each bar on x-axis
     x_values = date2num(selected_datetimes)
 
     #--- calculate the width of bar to be used to draw the bar graph on the x-axis
@@ -66,48 +64,25 @@ def plot_predictions_24hr (predicted_values, target_values, datetimes, str_selec
     return
     
 
-def plot_predictions_bar(predicted_values, target_values, datetimes):
+def plot_predictions(predicted_values, target_values, datetimes):
 
-    #---conver datetimest (['date_str', hr]) into datetime format
-    datetimes = [datetime.strptime(date_str + ' ' + str(hr), "%Y-%m-%d %H") for [date_str, hr] in datetimes]
+    #--- draw side-by-side bars for predicted and target values
+    _, ax  = plt.subplots(figsize=figsize)
+    ax.plot(datetimes, target_values, label='Actual')
+    ax.plot(datetimes, predicted_values, label='Predicted')
     
-    fig, ax = plt.subplots(figsize=figsize)
-    
-    ax.grid(True, which='both', axis='y')
-
-    x_values = date2num(datetimes)
-    num_ticks = len(x_values)
-    tick_width = 1 / num_ticks * (x_values[-1] - x_values[0])
-    padding = 5
-    bar_width = tick_width / 2 * 0.8 #--- 2 bars per tick (predicted bar and actual bar)
-    ax.bar(x_values - bar_width / 2, predicted_values, width=bar_width, label='Predicted')
-    ax.bar(x_values + bar_width / 2, target_values, width=bar_width, label='Actual')
-    ax.set_xlim(x_values[0] - bar_width, x_values[-1] + bar_width)
-
-
+    #--- format the x axis ticks
+    ax.set_xlim(datetimes[0], datetimes[-1])
     ax.xaxis_date()
-    ax.xaxis.set_tick_params(which='major', pad=15)    
-    ax.xaxis.set_major_locator(mdates.DayLocator())
-    ax.xaxis.set_major_formatter(mdates.DateFormatter("%b-%d"))
-    ax.xaxis.set_minor_locator(mdates.HourLocator())
-    ax.xaxis.set_minor_formatter(mdates.DateFormatter("%H"))
-    fig.autofmt_xdate(rotation=90, ha='center')    
+    date_locator = mdates.AutoDateLocator()
+    ax.xaxis.set_major_locator(date_locator)
+    ax.xaxis.set_major_formatter(mdates.AutoDateFormatter(date_locator))
     
+    #--- display rest of plot data
     plt.legend()
-    plt.title("Predicted v. Actual Bike Usage")
-    plt.xlabel("Date/Hour")
-    plt.ylabel("Bike Usage")
-    
-    return
-
-    
-def plot_predictions_line(predicted_values, target_values):
-
-    plt.figure(figsize=figsize)
-    plt.plot(target_values, label='Actual')
-    plt.plot(predicted_values, label='Predicted')
-    plt.legend()
-    plt.title("Predicted v. Actual Bike Usage")
+    plt.title("Predicted vs. Actual Bike Usage")
     plt.xlabel("Date")
     plt.ylabel("Bike Usage")
+    plt.grid(True, axis='y')
+    
     return
